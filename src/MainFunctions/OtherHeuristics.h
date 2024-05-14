6161952b87b09755d9675cd98b001aafc542f448
Manjeri998 <nunopinhofernandes@gmail.com>
2024-05-14 21:53:37 +0100

    m

--- src/MainFunctions/OtherHeuristics.h
+++ src/MainFunctions/OtherHeuristics.h
@@ -11,6 +11,8 @@ public:
     pair<int,vector<pair<int,Vertex *>>> tsp_dynamic_programming(vector<vector<double>> distances, vector<pair<int,Vertex *>> vertices, int pos, int mask, int k, pair<int,vector<pair<int, Vertex *>>> path);
     double haversine(double lat1, double lon1, double lat2, double lon2);
     pair<int,vector<pair<int, Vertex *>>> joinSets(pair<int,vector<pair<int, Vertex *>>> set1, pair<int,vector<pair<int, Vertex *>>> set2, bool toy);
+    int OtherHeuristic(DataManager aux, int k, bool toy);
+    vector<vector<double>> getDistances(vector<pair<int, Vertex*>> vertices, bool toy);
 };
 
 
