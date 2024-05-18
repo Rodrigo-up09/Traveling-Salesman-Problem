fc9ada14ca5a0afe8873fcb28145962f2252e1af
Rodrigo-up09 <rodriribeiro2004@gmail.com>
2024-05-18 19:15:06 +0100

    menu change

--- src/Menu/menu.cpp
+++ src/Menu/menu.cpp
@@ -27,150 +27,175 @@
 void displayMenu() {
     int dataSet, type;
 
-    cout << "Select Dataset:" << endl;
-    cout << "0 - Small" << endl;
-    cout << "1 - Medium" << endl;
-    cout << "2 - Real" << endl;
-    cout << "Enter your choice: ";
-    cin >> dataSet;
 
-    switch (dataSet) {
-        case 0:
+    while (true) {
+        cout << "Select Dataset:" << endl;
+        cout << "0 - Small" << endl;
+        cout << "1 - Medium" << endl;
+        cout << "2 - Real" << endl;
+        cout << "Enter your choice: ";
+        cin >> dataSet;
+
+        if (cin.fail() || dataSet < 0 || dataSet > 2) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid choice. Please try again." << endl;
+            continue;
+        }
+
+        if (dataSet == 0) {
             cout << "Select Small Dataset Type:" << endl;
             cout << "0 - Shipping" << endl;
             cout << "1 - Stadiums" << endl;
             cout << "2 - Tourism" << endl;
             cout << "Enter your choice: ";
             cin >> type;
-            break;
-        case 1:
+
+            if (cin.fail() || type < 0 || type > 2) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid choice. Please try again." << endl;
+                continue;
+            }
+        } else if (dataSet == 1) {
             cout << "Enter the number of nodes (e.g., 25, 50, 75, etc.): ";
             cin >> type;
-            break;
-        case 2:
+
+            if (cin.fail() || type <= 0) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid number of nodes. Please try again." << endl;
+                continue;
+            }
+        } else if (dataSet == 2) {
             cout << "Select Real Dataset Type:" << endl;
             cout << "1 - Graph 1" << endl;
             cout << "2 - Graph 2" << endl;
             cout << "Enter your choice: ";
             cin >> type;
-            break;
-        default:
-            cout << "Invalid choice" << endl;
-            return;
+
+            if (cin.fail() || type < 1 || type > 2) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid choice. Please try again." << endl;
+                continue;
+            }
+        }
+
+        break;
     }
 
     DataManager dataManager;
     dataManager.start(dataSet, type);
     int option;
 
-    cout << "What you to test?" << endl;
-    cout << "0 - Single Algoritm" << endl;
-    cout << "1 - Compare Multiple" << endl;
-    cin >> option;
-    switch (option) {
-        case 0:
-            displaySingleAlgoritm(dataManager);
-            break;
-        case 1:
-          displayMultiple( dataManager);
-            break;
+    while (true) {
+        cout << "What do you want to test?" << endl;
+        cout << "0 - Single Algorithm" << endl;
+        cout << "1 - Compare Multiple" << endl;
+        cin >> option;
 
-        default:
-            cout<<"Wrong option";
-    }
-
-
-
-}
-
-void displaySingleAlgoritm(DataManager dataManager){
-    int algoritm;
-
-    cout << "Select Algoritm:" << endl;
-    cout << "0 - Bactrack" << endl;
-    cout << "1 - Triangular" << endl;
-    cout << "2 -Other Heuristic" << endl;
-    cout << "3 -Real Word" << endl;
-    cout << "Enter your choice: ";
-    cin >> algoritm;
-
-
-    switch (algoritm) {
-        case 0:
-            displayBackTrac(dataManager);
-
-            break;
-        case 1:
-            displayTriangular(dataManager);
-
-            break;
-        case 2:
-            //TODO
-
-
-
-            break;
-        case 3:{
-            string origin;
-            cout << "Chose the Sorce Vertex:" << endl;
-            cin >> origin;
-            displayReal( dataManager, origin);
-            break;
+        if (cin.fail() || option < 0 || option > 1) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid choice. Please try again." << endl;
+            continue;
         }
-        default:
-            cout<<"Wrong option";
-            return;
+
+        if (option == 0) {
+            displaySingleAlgoritm(dataManager);
+        } else if (option == 1) {
+            displayMultiple(dataManager);
+        }
+
+        break;
     }
-
-
 }
+void displaySingleAlgoritm(DataManager dataManager) {
+    int algorithm;
 
+    while (true) {
+        cout << "Select Algorithm:" << endl;
+        cout << "0 - Backtrack" << endl;
+        cout << "1 - Triangular" << endl;
+        cout << "2 - Other Heuristic" << endl;
+        cout << "3 - Real World" << endl;
+        cout << "Enter your choice: ";
+        cin >> algorithm;
 
+        if (cin.fail() || algorithm < 0 || algorithm > 3) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid choice. Please try again." << endl;
+            continue;
+        }
+
+        switch (algorithm) {
+            case 0:
+                displayBackTrac(dataManager);
+                break;
+            case 1:
+                displayTriangular(dataManager);
+                break;
+            case 2:
+                // TODO: Implement heuristic display function
+                break;
+            case 3: {
+                string origin;
+                cout << "Choose the Source Vertex: ";
+                cin >> origin;
+                displayReal(dataManager, origin);
+                break;
+            }
+        }
+        break;
+    }
+}
 
 
 
 void displayMultiple(DataManager dataManager) {
-    int algoritm;
-    cout << "Select one option:" << endl;
-    cout << "0 -Compare Light Algoritms(use small )" << endl;
-    cout << "1 -Compare Mid Algoritms(use mid Graph,Backtrack is not called )" << endl;
-    cout << "2 - Compare heavy algoritms (BackTrack its not called and Other Heuristic)" << endl;
-    cout << "Enter your choice: ";
-    cin >> algoritm;
-    DataManager dataManagerB;
-    DataManager dataManagerT;
-    DataManager dataManagerH;
-    DataManager dataManagerR;
-    dataManager.copy(dataManagerB);
-    dataManager.copy(dataManagerT);
-    dataManager.copy(dataManagerH);
-    dataManager.copy(dataManagerR);
+    int algorithm;
+    while (true) {
+        cout << "Select one option:" << endl;
+        cout << "0 - Compare Light Algorithms (use small)" << endl;
+        cout << "1 - Compare Mid Algorithms (use mid Graph, Backtrack is not called)" << endl;
+        cout << "2 - Compare Heavy Algorithms (Backtrack and Other Heuristic are not called)" << endl;
+        cout << "Enter your choice: ";
+        cin >> algorithm;
 
-    switch (algoritm) {
-        case 0:
-            displayTriangular(dataManagerT);
-            displayBackTrac(dataManagerB);
-            //TODO heuristic
-            displayReal(dataManagerR,"0");
-            break;
+        if (cin.fail() || algorithm < 0 || algorithm > 2) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid choice. Please try again." << endl;
+            continue;
+        }
 
+        DataManager dataManagerB, dataManagerT, dataManagerH, dataManagerR;
+        dataManager.copy(dataManagerB);
+        dataManager.copy(dataManagerT);
+        dataManager.copy(dataManagerH);
+        dataManager.copy(dataManagerR);
 
-
-        case 1:
-            displayTriangular(dataManagerT);
-            //TODO heuristic
-            displayReal(dataManagerR,"0");
-
-            break;
-        case 2:
-            displayTriangular(dataManagerT);
-            //TODO heuristic
-            displayReal(dataManagerR,"0");
-
-            break;
-        default:
-            cout<<"Wrong option";
-            break;
+        switch (algorithm) {
+            case 0:
+                displayTriangular(dataManagerT);
+                displayBackTrac(dataManagerB);
+                // TODO: Call heuristic display function
+                displayReal(dataManagerR, "0");
+                break;
+            case 1:
+                displayTriangular(dataManagerT);
+                // TODO: Call heuristic display function
+                displayReal(dataManagerR, "0");
+                break;
+            case 2:
+                displayTriangular(dataManagerT);
+                // TODO: Call heuristic display function
+                displayReal(dataManagerR, "0");
+                break;
+        }
+        break;
     }
 }
 
